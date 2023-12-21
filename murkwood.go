0ffdc92f34b8e8041a0bfdec209a282f38c51ae0
sjesun <71446111+sjesun@users.noreply.github.com>
2023-12-21 07:08:46 -0500

    Update murkwood.go

--- murkwood.go
+++ murkwood.go
@@ -5,19 +5,19 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math/rand"
-  "os"
-  "strings"
+	"os"
+	"strings"
 )
 
 
 // secureSeed() generates a cryptographically secure random seed to be used
-//   for password generation.
+// 	for password generation.
 func secureSeed() int64 {
-  var seed int64
+	var seed int64
 	err := binary.Read(rand.Reader, binary.BigEndian, &seed) // rand.Reader ensures cryptographically secure randomization
 	if err != nil {
-    fmt.Println("Unable to generate secure seed. Error:", err)
-    fmt.Println("Exiting...")
+		fmt.Println("Unable to generate secure seed. Error:", err)
+		fmt.Println("Exiting...")
 		os.Exit(0)
 	}
 	return seed
@@ -25,31 +25,34 @@ func secureSeed() int64 {
 
 
 // passGen(length, digits, special) creates the password using the seed generated
-//   by secureSeed()
-func passGen(length int, digits bool, special bool) string {
-  // Ensuring the generated password is at least 12 characters long
-  if length < 12 {
-    length = 12
-  }
+// 	by secureSeed()
+func passGen(length int, digits bool, special bool, exclude bool) string {
+	// Ensuring the generated password is at least 12 characters long
+	if length < 12 {
+		length = 12
+	}
   
 	seed := secureSeed()
 	rand.Seed(secureSeed)
-
-  // Defining characters for the password
-  var lower = "abcdefghijklmnopqrstuvwxyz"
+	
+	// Defining characters for the password
+	var lower = "abcdefghijklmnopqrstuvwxyz"
 	var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var chars = lower + upper
+	var chars = lower + upper
+	
+	if digits {
+		var nums = "0123456789"
+		chars += nums
+	}
+	
+	if special {
+		var symbols = "!@#$%^&*()_-+=<>?/{}[]|"
+		chars += symbols
+	}
 
-  if digits {
-    var nums = "0123456789"
-    chars += nums
-  }
-
-  if special {
-    var symbols = "!@#$%^&*()_-+=<>?/{}[]|"
-    chars += symbols
-  }
-
+	// exclude not implemented yet. When exclude is true, passGen will exclude certain 
+	// 	user-defined characters from chars.
+	
 	// Generate the password
 	password := make([]byte, length)
 	for i := 0; i < length; i++ {
