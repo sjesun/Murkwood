e1c6a233e924e6c1dadc694ab8cdd9cb919d6cc5
sjesun <71446111+sjesun@users.noreply.github.com>
2023-12-23 11:17:00 -0500

    Update murkwood.go

--- murkwood.go
+++ murkwood.go
@@ -31,27 +31,45 @@ func defineChars(addDigits bool, addSpecial bool, excludeChars bool, exCharStrin
 	var lower = "abcdefghijklmnopqrstuvwxyz"
 	var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	var chars = lower + upper
-	if addDigits {
+	if addDigits { // user wants to include digits
 		var nums = "0123456789"
 		chars += nums
 	}
-	if addSpecial {
+	if addSpecial { // user wants to include special characters
 		var symbols = "!@#$%^&*()_-+=<>?/{}[]|"
 		chars += symbols
 	}
 	if excludeChars {
-		excludeList := strings.Split(exCharString, "")
-		// implement dict/set
-		for _, item := range excludeList {
-			chars = strings.ReplaceAll(chars, item, "")
+		charList := strings.Split(chars, "")
+		charDict := make(map[string]bool) // to store the one-character strings from charList
+		
+		// populating charDict with characters
+		for character := range charList {
+			if present := charDict[character]; !present {
+				charDict[character] = true
+			}
 		}
+		// excluding characters from charDict
+		excludeList := strings.Split(exCharString, "")
+		for character := range excludeList {
+			if present := charDict[character]; present {
+				delete(charDict, character)
+			}
+		}
+		// updating chars
+		chars = ""
+		for key := range charDict {
+			chars += key
+		}
+		
 	}
+	return chars
 }
 
 
 // passGen(length, digits, special) creates the password using the seed generated
 // 	by secureSeed()
-func passGen(length int, digits bool, special bool, exclude bool) string {
+func passGen(length int, chars string) string {
 	// Ensuring the generated password is at least 12 characters long
 	if length < 12 {
 		length = 12
